ee2280550f74d2aa6993b7681b5ee1491b0f1d08
shiffygreen <shiffygreen73@gmail.com>
2023-11-19 12:28:46 -0500

    spec

---
+++ spec.sql
@@ -0,0 +1,48 @@
+/*
+I am part of the vaad and I am compiling a list of girls high schools in Lakewood NJ. I would like to be able to keep track of them very easily.  
+I am hoping that this list will be able to help the whole community with deciding about which school to send their daughters to. 
+
+I would need the name of the school, the address, the phone number, the name of the principal, the number of students in each grade, and the number of girls in the whole school. 
+In addition, I would like the amount the school charges for tuition and how much of a discount they usually give.  
+I would also like to be able to see if the school is growing or shrinking 
+
+
+Questions: 
+Q: Are there any schools with the same name? 
+A: No, there is a law in New jersey that you are not able to have two schools with the same name. 
+in addition, there is a law that every school must call the 9th grade freshies, 10th sophies, 11th juniors, 12th seniors. 
+I'm not sure why this is a law but that is how I need it in this system. 
+ 
+Q: Are there any schools that are in the same building? 
+A: Yes, there actually are two high schools in the same building. We don’t really recommend it but unfortunately this does take place. 
+
+Q: Do you need to know how much tuition is coming in per grade? 
+A: No, that does not give us a better picture of whether the school is financially viable or not. 
+
+Q: Do all phone numbers start with 732? 
+A: Yes they do 
+
+
+Reports: 
+
+1) List of how many schools are connected to an elementary school.  
+2) Parents want to know the size of the schools, since they want their daughters to be noticed. 
+   Show a list of each school’s number of students, from greatest to lowest. 
+3) I want to know which schools are financially secure.  
+     A) What is the total of the tuition money coming in per school? 
+     B)How does this compare to the school’s yearly budget? Show me a column for the schools that have more money coming in than spent, and for those that are in debt since their budget is more than their tuition 
+     C)Is the school’s budget increasing or decreasing by the year? 
+     D)Show me the two schools that seem to be the most secure based on this data 
+
+ 
+
+Sample data is in this order: Name of the school, address, phone number, principal name, each grade with the number of girls in it from 9th to 12th, tuition charged, discounted tuition, percentage of parents paying full tuition, connected to an elementary school, yearly budget  
+
+ 
+Oros Bais Yaakov, 1995 Rutgers University Blvd, 370-6049, Mrs. Tamara Epstein, 140, 145, 150, 125, $4500, $3000, 75%, no, $2,158,000 
+Meiras Bais Yaakov, 500 Vermont Ave, 886-5854, Rebbitzen Paretzky, 107, 94, 63, null, $4200, $2900, 45%, no, $1,600,500 
+Bais Leah, 8745 Lanes Mill Rd, 773-8796, Mrs. Yocheved Miller, 73, 85, 80, 77, $4100, $1555, 74%, yes, $2,459,120 
+Shiras Devorah, 436 Oak Street, 961-1283, Mrs. Devorah Feifer, 170, 165, 160, 155, $5500, $2000, 90%, yes, $2,400,000 
+Bais Yaakov, 452 James Street, 508-0138, Mrs. Sori Schechter, 167, 160, 150, 155, $4000, $3000, 75%, no, $2,100,000 
+Bais Fishy, 436 Oak Street, 569-1314, Mrs. Shiffy Green, 110, 105, 100, 95, $10000, $5999, 100%, no, $1,000,000 
+*/
