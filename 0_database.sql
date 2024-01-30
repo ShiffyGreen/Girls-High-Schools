9a8876d84c91349631e26d68fcf184a88473411f
sarigettinger <sarigettinger@gmail.com>
2024-01-30 18:54:05 -0500

    merged changes

--- 0_database.sql
+++ 0_database.sql
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 use master 
 drop database if exists GirlsHighSchoolsDB
 go
 create database GirlsHighSchoolsDB
 go
-=======
-use master 
-drop database if exists GirlsHighSchoolsDB
-go
-create database GirlsHighSchoolsDB
-go
->>>>>>> fd79262e87a269a981226090ca5149d3249d6cc8
--- 3_reports_girlshighschools.sql
+++ 3_reports_girlshighschools.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use GirlsHighSchoolsDB
 go
 --1)
@@ -15,17 +14,3 @@ order by g.TotalStudentBody desc
 select TotalTuitionPerSchool = (g.FullTuition * g.PercentagePayingFull * .01 * g.TotalStudentBody) + (g.DiscountedTuition * (100-g.PercentagePayingFull) * .01 * g.TotalStudentBody), SchoolName
 from GirlsHighSchools g
 group by SchoolName, (g.FullTuition * g.PercentagePayingFull * .01 * g.TotalStudentBody) + (g.DiscountedTuition * (100-g.PercentagePayingFull) * .01 * g.TotalStudentBody)
-=======
---1)
-select * from GirlsHighSchools g
-where g.ConnectedToElementary = 'yes'
-
---2)
-select g.TotalStudentBody from GirlsHighSchools g
-order by g.TotalStudentBody desc
-
---3)
-select TotalTuitionPerSchool = (g.FullTuition * g.PercentagePayingFull * .01 * g.TotalStudentBody) + (g.DiscountedTuition * (100-g.PercentagePayingFull) * .01 * g.TotalStudentBody), SchoolName
-from GirlsHighSchools g
-group by SchoolName, (g.FullTuition * g.PercentagePayingFull * .01 * g.TotalStudentBody) + (g.DiscountedTuition * (100-g.PercentagePayingFull) * .01 * g.TotalStudentBody)
->>>>>>> fd79262e87a269a981226090ca5149d3249d6cc8
