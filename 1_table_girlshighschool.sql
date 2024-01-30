a394d9cb68d3a15c701817a91131645ae5f5845f
sarigettinger <sarigettinger@gmail.com>
2024-01-29 23:47:40 -0500

    fixed old changes:)

---
+++ 1_table_girlshighschool.sql
@@ -0,0 +1,52 @@
+use GirlsHighSchoolsDB
+go
+drop table if exists GirlsHighSchools
+go
+create table dbo.GirlsHighSchools
+(
+    SchoolName varchar (35) not null 
+        constraint u_school_name_must_be_unique unique (SchoolName)
+        constraint ck_SchoolName_cannot_be_blank check (SchoolName <> ''),
+    SchoolAddress varchar (75) not null
+        constraint ck_SchoolAddress_cannot_be_blank check (SchoolAddress <> ''),
+    SchoolPhoneNumber varchar (15) not null
+        constraint ck_SchoolPhoneNumber_begins_with_732 check (SchoolPhoneNumber like '(732)%'),
+    SchoolPrincipal varchar (40) not null 
+        constraint ck_SchoolPrincipal_cannot_be_blank check (SchoolPrincipal <> '')
+        constraint ck_SchoolPrincipal_must_be_unique unique (SchoolPrincipal),
+    NumOfFreshies int not null 
+        constraint ck_NumofFreshies_positive check (NumOfFreshies > 0),
+    NumOfSophies int
+        constraint ck_NumofSophies_positive check (NumOfSophies > 0),
+    NumOfJuniors int
+        constraint ck_NumofJuniors_positive check (NumOfJuniors > 0),
+    NumOfSeniors int
+        constraint ck_NumofSeniors_positive check (NumOfSeniors > 0),
+    FullTuition decimal (10, 2) not null 
+        constraint ck_FullTuition_positive check (FullTuition > 0),
+    DiscountedTuition decimal (10, 2) not null 
+        constraint ck_DiscountedTuition_positive check (DiscountedTuition > 0),
+    PercentagePayingFull int not null 
+        constraint ck_percentage_paying_full_between_0_and_100 check (PercentagePayingFull between 0 and 100),
+    ConnectedToElementary  varchar (3) not null
+        constraint ck_Connected_to_Elementary_yes_or_no check (ConnectedToElementary in ('yes', 'no')),
+    YearlyBudget decimal (20, 2) not null
+    constraint ck_school_budget_positive check (YearlyBudget > 0),
+    TotalStudentBody as case 
+    when NumOfSophies is null then NumOfFreshies
+    when NumOfSophies is not null and NumOfJuniors is null then NumOfFreshies + NumOfSophies
+    when NumOfJuniors is not null and NumOfSeniors is null then NumOfFreshies + NumOfSophies + NumOfJuniors
+    else NumOfFreshies + NumOfSophies + NumOfJuniors + NumOfSeniors
+    end
+    persisted,
+    GrowingOrShrinking as case 
+    when NumOfFreshies < NumOfSophies and NumOfSophies < NumOfJuniors  and NumOfJuniors < NumOfSeniors
+    then 'shrinking'
+    when (NumOfFreshies > NumOfSophies or NumOfSophies is null)  
+    and (NumOfSophies > NumOfJuniors or NumOfJuniors is null)  
+    and (NumOfJuniors > NumOfSeniors  or NumOfSeniors is null)  then 'growing'
+    else 'neither' end
+    persisted
+    )
+go
+
---
+++ 2_data_girlshighschools.sql
@@ -0,0 +1,14 @@
+use GirlsHighSchoolsDB
+go
+
+delete GirlsHighSchools
+go
+
+insert GirlsHighSchools (SchoolName, SchoolAddress, SchoolPhoneNumber, SchoolPrincipal, NumOfFreshies, NumOfSophies, NumOfJuniors, NumOfSeniors, FullTuition, DiscountedTuition, PercentagePayingFull, ConnectedToElementary, YearlyBudget)
+select 'Oros Bais Yaakov', '1995 Rutgers University Blvd', '(732) 370-6049', 'Mrs. Tamara Epstein', 140, 145, 150, 125, 4500, 3000, 75, 'no', 2158000 
+union select 'Meiras Bais Yaakov', '500 Vermont Ave', '(732) 886-5854', 'Rebbitzen Paretzky', 107, 94, 63, null, 4200, 2900, 45, 'no', 1600500 
+union select 'Bais Leah', '8745 Lanes Mill Rd', '(732) 773-8796', 'Mrs. Yocheved Miller', 73, 85, 80, 77, 4100, 1555, 74, 'yes', 2459120 
+union select 'Shiras Devorah', '436 Oak Street', '(732) 961-1283', 'Mrs. Devorah Feifer', 170, 165, 160, 155, 5500, 2000, 90, 'yes', 2400000 
+union select 'Bais Yaakov', '452 James Street','(732) 508-0138', 'Mrs. Sori Schechter', 167, 160, 150, 155, 4000, 3000, 75, 'no', 2100000 
+union select 'Bais Fishy', '436 Oak Street', '(732) 569-1314', 'Mrs. Shiffy Green', 110, 105, 100, 95, 10000, 5999, 100, 'no', 1000000 
+go
